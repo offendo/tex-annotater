ba6ba26d18a9e3c97760309864c0bc819b842c0a
offendo <nilaypatel2@gmail.com>
2024-04-02 16:24:32 -0700

    Add autosaves (#11)
    * Trying to fix pdfs
    
    * Fix slow pdf rendering (kinda)

--- src/backend/data.py
+++ src/backend/data.py
@@ -46,8 +46,7 @@ def list_s3_documents():
     names = [d["Key"].split("/")[-1] for d in docs if d["Key"].startswith("texs/")]
     return names
 
-
-# @filecache(maxsize=5)
+  
 def load_pdf(file_name):
     # obj = session.get_object(Bucket="tex-annotation", Key=f"pdfs/{file_name}")
     # data = obj["Body"].read()
