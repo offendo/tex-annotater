33e18fc93b6f3dc0f63cbd2611c60fd905bef540
Nilay <nilaypatel2@gmail.com>
2024-04-29 17:46:44 +0000

    add logging to file in gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,3 +6,8 @@ for env_file in ('.env', '.flaskenv'):
     env = os.path.join(os.getcwd(), env_file)
     if os.path.exists(env):
         load_dotenv(env)
+
+loglevel = 'debug'
+accesslog = '/tmp/log/gunicorn/access_log_tex'
+acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
+errorlog =  '/tmp/log/gunicorn/error_log_tex'
