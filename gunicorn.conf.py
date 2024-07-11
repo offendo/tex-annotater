ebcd0bbb5c1e9f42092aa44eb99cc805b3d2c12a
Nilay <nilaypatel2@gmail.com>
2024-07-11 23:25:54 +0000

    update some gunicorn stuff

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,21 @@
 # gunicorn.conf.py
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
+# Source the env files
 for env_file in ('.env', '.flaskenv'):
     env = os.path.join(os.getcwd(), env_file)
     if os.path.exists(env):
         load_dotenv(env)
 
+# Make the log directory
+Path('/tmp/log/gunicorn').mkdir(exist_ok=True, parents=True)
+
+# gunicorn files
 loglevel = 'debug'
-accesslog = '/tmp/log/gunicorn/access_log_tex'
+#accesslog = '/tmp/log/gunicorn/access_log_tex'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog =  '/tmp/log/gunicorn/error_log_tex'
+#errorlog =  '/tmp/log/gunicorn/error_log_tex'
+errorlog = '-'
+accesslog = '-'
