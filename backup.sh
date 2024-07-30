3c1bbbcfbc31337d08de9de3939c2c012d8e60c4
Nilay <nilaypatel2@gmail.com>
2024-07-30 22:25:01 +0000

    adjust backup script

--- backup.sh
+++ backup.sh
@@ -4,10 +4,6 @@ DB_NAME=annotations-db
 DB_USER=postgres
 DB_PASS=password
 
-TIMESTAMP=$(date +%F_%T | tr ':' '-')
-TEMP_FILE=$(mktemp tmp.XXXXXXXXXX)
-S3_FILE="s3://$BUCKET_NAME/backup-$TIMESTAMP"
-
 BUCKET_NAME=tex-annotation
 
 TIMESTAMP=$(date +%F_%T | tr ':' '-')
@@ -16,4 +12,6 @@ S3_FILE="s3://$BUCKET_NAME/database-backup/backup-$TIMESTAMP"
 
 docker-compose run postgres pg_dump -Fc --no-acl -h postgres -U $DB_USER $DB_NAME > $TEMP_FILE
 aws s3 cp $TEMP_FILE $S3_FILE --sse
+
+sleep(30)
 rm "$TEMP_FILE"
