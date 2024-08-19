718630a24075f594b35d9375fc1573ce90d546bd
Nilay <nilaypatel2@gmail.com>
2024-08-19 21:17:45 +0000

    update backup script, which still doesn't work right

--- backup.sh
+++ backup.sh
@@ -10,8 +10,8 @@ TIMESTAMP=$(date +%F_%T | tr ':' '-')
 TEMP_FILE=$(mktemp tmp.XXXXXXXXXX)
 S3_FILE="s3://$BUCKET_NAME/database-backup/backup-$TIMESTAMP"
 
-docker-compose run postgres pg_dump -Fc --no-acl -h postgres -U $DB_USER $DB_NAME > $TEMP_FILE
+docker-compose -f /home/ec2-user/tex-annotater/docker-compose.yaml run postgres pg_dump -Fc --no-acl -h postgres -U $DB_USER $DB_NAME > $TEMP_FILE
 aws s3 cp $TEMP_FILE $S3_FILE --sse
 
-sleep 30
+sleep 5
 rm "$TEMP_FILE"
