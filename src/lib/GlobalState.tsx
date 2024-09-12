4942e42b004e154b6d5c11baf2ae2eddaaf28d8b
Nilay Patel <nilaypatel2@gmail.com>
2024-09-12 14:51:41 -0700

    Fix edit button (?)

--- src/lib/GlobalState.tsx
+++ src/lib/GlobalState.tsx
@@ -315,7 +315,7 @@ export const toggleEditStatus = (state: GlobalStateProps, anno: TextSpan | null)
 }
 
 export const updateMark = (state: GlobalStateProps, anno: TextSpan) => {
-    const newAnnos = toggle(state.annotations, anno, (a, b) => a.start == b.start && a.end == b.end && a.tag == b.tag, true);
+    const newAnnos = toggle(state.annotations, anno, (a, b) => a.annoid == b.annoid, true);
     updateAnnotations(state, newAnnos);
     state.setEditing(null);
 }
