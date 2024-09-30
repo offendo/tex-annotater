aab21d52e065e0ca734929ba00eb5f41803dde77
Nilay Patel <nilaypatel2@gmail.com>
2024-09-30 10:45:40 -0700

    Fix the tokenized autoLink function

--- src/lib/components/Annotater/Annotater.tsx
+++ src/lib/components/Annotater/Annotater.tsx
@@ -11,9 +11,14 @@ import {
   getViewerWidthInChars,
   displaySplitsWithDiff,
 } from "@/lib/utils";
-import { ColorMap, colors } from "@/lib/colors"
+import { ColorMap, colors } from "@/lib/colors";
 import sortBy from "lodash.sortby";
-import { GlobalState, toggleLink, updateAnnotations, updateMark } from "@/lib/GlobalState";
+import {
+  GlobalState,
+  toggleLink,
+  updateAnnotations,
+  updateMark,
+} from "@/lib/GlobalState";
 
 type AnnotaterProps = {
   style: any;
@@ -31,7 +36,7 @@ const getNextColor = function (start_color: string = "") {
     idx = 0;
   }
   return items[idx];
-}
+};
 
 /**
  * Annotater element which houses the source code and handles the highlight event (handleMouseUp).
@@ -42,10 +47,11 @@ const Annotater = (props: AnnotaterProps) => {
 
   /* State */
   const state = useContext(GlobalState);
-  const [currentSelection, setCurrentSelection] = useState<Selection | null>(null);
+  const [currentSelection, setCurrentSelection] = useState<Selection | null>(
+    null,
+  );
   const [currentColor, setCurrentColor] = useState<string>("");
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
-
 
   /** Add annotation to document */
   const addMark = (start: number, end: number, label: string, name: string) => {
@@ -53,7 +59,7 @@ const Annotater = (props: AnnotaterProps) => {
 
     // If it doesn't already exist in the annotations, add it
     const nextColor = getNextColor(currentColor);
-    setCurrentColor(nextColor)
+    setCurrentColor(nextColor);
     const newSpan = {
       annoid: crypto.randomUUID(),
       start: start,
@@ -63,10 +69,12 @@ const Annotater = (props: AnnotaterProps) => {
       name: name,
       fileid: state.fileid,
       links: [],
-      color: nextColor
+      color: nextColor,
     } as TextSpan;
 
-    const splitIndex = state.annotations.findIndex((s) => s.end == end && s.start == start && s.tag == label);
+    const splitIndex = state.annotations.findIndex(
+      (s) => s.end == end && s.start == start && s.tag == label,
+    );
     if (splitIndex == -1) {
       updateAnnotations(state, [...state.annotations, newSpan]);
     }
@@ -93,7 +101,14 @@ const Annotater = (props: AnnotaterProps) => {
 
     // Checks if the click is within the bounding box of the selected text; otherwise, doesn't bring it up
     const BOUND = 50;
-    if (!(bb.left - BOUND <= e.pageX && e.pageX <= bb.right + BOUND && bb.top - BOUND <= e.pageY && e.pageY <= bb.bottom + BOUND)) {
+    if (
+      !(
+        bb.left - BOUND <= e.pageX &&
+        e.pageX <= bb.right + BOUND &&
+        bb.top - BOUND <= e.pageY &&
+        e.pageY <= bb.bottom + BOUND
+      )
+    ) {
       return;
     }
 
@@ -104,32 +119,39 @@ const Annotater = (props: AnnotaterProps) => {
     setSelectionClicked(true);
 
     // Move the menu to the right location
-    const newPos = { x: e.pageX, y: e.pageY }
+    const newPos = { x: e.pageX, y: e.pageY };
     setCursorPos(newPos);
   };
 
   const autoLink = (anno: TextSpan) => {
-
     /* ------------------------------------------------------------------
      *  If it's a name which is inside a definition or theorem, link it
      * -------------------------------------------------------------------
      */
-    if (anno.tag == 'name') {
+    if (anno.tag == "name") {
       let target: TextSpan | null = null;
       const isInside = (anno: TextSpan, defOrThm: TextSpan) => {
         return anno.start >= defOrThm.start && anno.end <= defOrThm.end;
-      }
+      };
       for (const anno_b of state.annotations) {
-        if ((anno_b.tag == "definition" || anno_b.tag == "theorem" || anno_b.tag == "example") && isInside(anno, anno_b)) {
+        if (
+          (anno_b.tag == "definition" ||
+            anno_b.tag == "theorem" ||
+            anno_b.tag == "example") &&
+          isInside(anno, anno_b)
+        ) {
           // we want the most-inner definition
-          if (target == null || (target.end - target.start) > (anno_b.end - anno_b.start)) {
+          if (
+            target == null ||
+            target.end - target.start > anno_b.end - anno_b.start
+          ) {
             target = anno_b;
           }
         }
       }
       // If there's a possible target, add it as a link
       if (target != null) {
-        toggleLink(state, anno, target, true)
+        toggleLink(state, anno, target, true);
       }
     }
 
@@ -142,27 +164,32 @@ const Annotater = (props: AnnotaterProps) => {
       if (name == ref) {
         return true;
       }
-      // 2. Check if it matches a plural
-      if (ref == name + 's' || ref == name + 'es') {
+      // 2. Check if ref is a plural of name
+      if (ref == name + "s" || ref == name + "es") {
         return true;
       }
       // 3. Check if any of the individual words match
-      for (const token of name.split(' ')) {
-        if (token == ref) {
+      for (const token of ref.split(" ")) {
+        if (token == name || token == name + "s" || token == name + "es") {
           return true;
         }
       }
-    }
-    if (anno.tag == 'reference') {
+    };
+    if (anno.tag == "reference") {
       // filter
       // 1. names
       // 2. which occur before the reference
       // 3. with the same text as the reference
       // and then sort by start (in reverse order)
       const candidates: TextSpan[] = sortBy(
-        state.annotations.filter((a) => a.tag == 'name' && a.end <= anno.start && _nameMatch(a.text, anno.text)),
-        (o: TextSpan) => -o.start
-      )
+        state.annotations.filter(
+          (a) =>
+            a.tag == "name" &&
+            a.end <= anno.start &&
+            _nameMatch(a.text, anno.text),
+        ),
+        (o: TextSpan) => -o.start,
+      );
 
       // If we have any candidates, go for it.
       if (candidates.length > 0) {
@@ -174,34 +201,45 @@ const Annotater = (props: AnnotaterProps) => {
      *  If it's a proof, link it to the previous theorem name within 500 characters
      * -----------------------------------------------------------------------------
      */
-    if (anno.tag == 'proof') {
+    if (anno.tag == "proof") {
       // filter
       // 1. theorems
       // 2. which occur before the proof
       // 3. within 500 characters
       // and then sort by closest distance
       const candidates: TextSpan[] = sortBy(
-        state.annotations.filter((a) =>
-          a.tag == 'theorem'
-          && a.end <= anno.start
-          && a.end + 500 >= anno.start
+        state.annotations.filter(
+          (a) =>
+            a.tag == "theorem" &&
+            a.end <= anno.start &&
+            a.end + 500 >= anno.start,
         ),
-        (o: TextSpan) => -(o.start - anno.start)
-      )
+        (o: TextSpan) => -(o.start - anno.start),
+      );
 
       // If we have any candidates, go for it.
       if (candidates.length > 0) {
         toggleLink(state, anno, candidates[0], true);
       }
     }
+  };
 
-  }
-
-  const handleContextMenuButtonPress = (start: number, end: number, label: string, name: string) => {
+  const handleContextMenuButtonPress = (
+    start: number,
+    end: number,
+    label: string,
+    name: string,
+  ) => {
     // if we're editing something, just update the mark instead
     let newAnno = null;
     if (state.editing != null) {
-      newAnno = { ...state.editing, start: start, end: end, tag: label, text: state.tex.slice(start, end) };
+      newAnno = {
+        ...state.editing,
+        start: start,
+        end: end,
+        tag: label,
+        text: state.tex.slice(start, end),
+      };
       updateMark(state, newAnno);
     } else {
       newAnno = addMark(start, end, label, name);
@@ -222,25 +260,34 @@ const Annotater = (props: AnnotaterProps) => {
       case "Escape":
         setSelectionClicked(false);
         const selection = window.getSelection();
-        if (selection != null) { selection.empty(); }
+        if (selection != null) {
+          selection.empty();
+        }
         break;
     }
-  }
+  };
 
-  const splits = props.diff && props.annotations
-    ? displaySplitsWithDiff(state.tex, props.annotations, props.diff)
-    : displaySplits(state.tex, props.annotations ? props.annotations : state.annotations);
+  const splits =
+    props.diff && props.annotations
+      ? displaySplitsWithDiff(state.tex, props.annotations, props.diff)
+      : displaySplits(
+          state.tex,
+          props.annotations ? props.annotations : state.annotations,
+        );
 
   // Return the formatted code
   return (
     <div>
-      <div className="tex-container" tabIndex={-1} onKeyUp={handleSelectionKeyPress} >
+      <div
+        className="tex-container"
+        tabIndex={-1}
+        onKeyUp={handleSelectionKeyPress}
+      >
         <pre id="viewer" style={{ whiteSpace: "pre-wrap" }}>
           <div style={props.style} onMouseUp={launchContextMenu}>
             {splits.map((split) => (
               <Split key={`${split.start}-${split.end}`} {...split} />
-            )
-            )}
+            ))}
           </div>
         </pre>
       </div>
@@ -249,8 +296,13 @@ const Annotater = (props: AnnotaterProps) => {
           top={cursorPos.y - 10}
           left={cursorPos.x - 10}
           range={parseSelection(currentSelection)}
-          onButtonPress={(e, label, name, start, end) => { e.preventDefault(); handleContextMenuButtonPress(start, end, label, name) }}
-          onMouseLeave={(e) => { setSelectionClicked(false); }}
+          onButtonPress={(e, label, name, start, end) => {
+            e.preventDefault();
+            handleContextMenuButtonPress(start, end, label, name);
+          }}
+          onMouseLeave={(e) => {
+            setSelectionClicked(false);
+          }}
         />
       )}
     </div>
